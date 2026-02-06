627d0989901156aa9450698e2ed17a2ab186bfb0
limheewoo <heewoo39@gmail.com>
2026-02-06 21:11:56 +0900

    Chore: svgr/webpack 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,64 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: [
+            {
+              loader: "@svgr/webpack",
+              options: { dimensions: false, icon: true },
+            },
+          ],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  webpack(config) {
+    // 1. 기존의 SVG 로더 규칙을 찾습니다.
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.(".svg")
+    );
+
+    if (fileLoaderRule) {
+      config.module.rules.push(
+        // 2. ?url 쿼리가 붙은 경우에만 기존 로더(이미지 경로)를 사용하게 합니다.
+        {
+          ...fileLoaderRule,
+          test: /\.svg$/i,
+          resourceQuery: /url/,
+        },
+        // 3. 그 외의 모든 .svg 파일은 SVGR이 컴포넌트로 변환하게 합니다.
+        {
+          test: /\.svg$/i,
+          issuer: fileLoaderRule.issuer,
+          resourceQuery: {
+            not: [...(fileLoaderRule.resourceQuery?.not || []), /url/],
+          },
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                dimensions: false,
+                icon: true,
+                // replaceAttrValues: {
+                //   "#000": "currentColor",
+                //   black: "currentColor",
+                // },
+              },
+            },
+          ],
+        }
+      );
+
+      // 4. 기존의 포괄적인 SVG 규칙에서는 .svg를 제외시킵니다.
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
